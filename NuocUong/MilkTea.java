15f0dbc40e03059bac208af87317a9a9fb32d133
Nguyễn Nam Dương <3123560012@sv.sgu.edu.vn>
2024-11-27 17:26:09 +0700

    Update display

--- NuocUong/MilkTea.java
+++ NuocUong/MilkTea.java
@@ -6,5 +6,22 @@ package NuocUong;
 public class MilkTea extends DrinkingWater {
     private int price;
     private String size;
-    
+
+    public MilkTea() {
+        this.price = 0;
+        this.size = "";
+    }
+
+    public MilkTea(int price, String size) {
+        this.price = price;
+        this.size = size;
+    }
+
+    // Getter - Setter
+    public int getPrice() {
+        return price;
+    }
+    public String getSize() {
+        return size;
+    }
 }
