80d7d942b9eac3acac1c1df757e9ee53fe440b5e
tien luan <tienluanchenh192@gmail.com>
2024-11-30 11:47:05 +0700

    customer

--- Customer/Customer.java
+++ Customer/Customer.java
@@ -16,13 +16,13 @@ public class Customer {
     private static int numOfCustomer = 0;
 
     public Customer() {
-        this.customerID = "KH" + numOfCustomer++;
+        this.customerID = "KH" + ++numOfCustomer;
         this.birthDay = new Date();
         this.address = new Address();
     }
 
     public Customer(String customerName, Date birthday, Address address) {
-        this.customerID = "KH" + numOfCustomer++;
+        this.customerID = "KH" + ++numOfCustomer;
         this.customerName = customerName;
         this.birthDay = birthday;
         this.age = birthday.calcAge();
@@ -66,8 +66,8 @@ public class Customer {
         System.out.println("[SetInfo] Thêm thông tin cho [Khách hàng] có ID: " + this.customerID);
 
         while (true) { 
-            String str = sc.nextLine();
             System.out.print("[Input] Nhập tên khách hàng: ");
+            String str = sc.nextLine();
 
             if (Function.isEmpty(str)) {
                 System.out.println("[Warning] Tên khách hàng không được rỗng!");
@@ -84,8 +84,8 @@ public class Customer {
         }
 
         while (true) { 
+            System.out.print("[Input] Nhập ngày sinh khách hàng: ");
             String str = sc.nextLine();
-            System.out.println("[Input] Nhập ngày sinh khách hàng: ");
 
             if (Function.isEmpty(str)) {
                 System.out.println("[Warning] Ngày sinh khách hàng không được rỗng!");
@@ -96,6 +96,103 @@ public class Customer {
                 System.out.println("[Warning] Ngày sinh khách hàng phải là số!");
                 continue;
             }
+
+            this.birthDay.setDay(str);
+            break;
         }
+
+        while (true) { 
+            System.out.print("[Input] Nhập tháng sinh khách hàng: ");
+            String str = sc.nextLine();
+
+            if (Function.isEmpty(str)) {
+                System.out.println("[Warning] Tháng sinh khách hàng không được rỗng!");
+                continue;
+            }
+
+            if (!Function.isTrueNumber(str)) {
+                System.out.println("[Warning] Tháng sinh khách hàng phải là số!");
+                continue;
+            }
+
+            this.birthDay.setMonth(str);
+            break;
+        }
+
+        while (true) { 
+            System.out.print("[Input] Nhập năm sinh khách hàng: ");
+            String str = sc.nextLine();
+
+            if (Function.isEmpty(str)) {
+                System.out.println("[Warning] Năm sinh khách hàng không được rỗng!");
+                continue;
+            }
+
+            if (!Function.isTrueNumber(str)) {
+                System.out.println("[Warning] Năm sinh khách hàng phải là số!");
+                continue;
+            }
+
+            this.birthDay.setYear(str);
+            break;
+        }
+
+        this.age = this.birthDay.calcAge();
+
+        while (true) { 
+            System.out.print("[Input] Nhập số nhà của khách hàng: ");
+            String str = sc.nextLine();
+
+            if (Function.isEmpty(str)) {
+                System.out.println("[Warning] Số nhà khách hàng không được rỗng!");
+                continue;
+            }
+
+            this.address.setHouseNumer(str);
+            break;
+        }
+
+        while (true) { 
+            System.out.print("[Input] Nhập phường/xã của khách hàng: ");
+            String str = sc.nextLine();
+
+            if (Function.isEmpty(str)) {
+                System.out.println("[Warning] Phường/xã của khách hàng không được rỗng!");
+                continue;
+            }
+
+            this.address.setWardName(str.trim());
+            break;
+        }
+
+        while (true) { 
+            System.out.print("[Input] Nhập quận/huyện của khách hàng: ");
+            String str = sc.nextLine();
+
+            if (Function.isEmpty(str)) {
+                System.out.println("[Warning] Quận/huyện của khách hàng không được rỗng!");
+                continue;
+            }
+
+            this.address.setCityName(str.trim());
+            break;
+        }
+
+        this.address.getProvince().setInfo();
+    }
+
+    public void displayInfo() {
+        System.out.println("========== [Thông tin khách hàng] ==========");
+        System.out.println("[ID]: " + this.customerID);
+        System.out.println("[Tên khách hàng]: " + this.customerName);
+        System.out.println("[Ngày sinh]: " + this.birthDay.toString());
+        System.out.println("[Tuổi]: " + this.age);
+        System.out.println("[Địa chỉ]: " + this.address.toString());
+    }
+
+    public static void main(String[] args) {
+        Customer a = new Customer();
+        a.setInfo();
+        a.displayInfo();
     }
 }
