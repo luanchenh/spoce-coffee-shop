b3a67ae5bf83901723ae6922e9a105c066285af1
tien luan <tienluanchenh192@gmail.com>
2024-12-08 18:24:00 +0700

    nguoi dung

--- FinalJava/NguoiDung.java
+++ FinalJava/NguoiDung.java
@@ -1,15 +1,15 @@
 package FinalJava;
 
-import java.io.File;
-import java.util.Scanner;
-
+import Ban.Ban;
+import Ban.QLBan;
 import KhachHang.KHMangDi;
 import KhachHang.KHTaiCho;
 import KhachHang.KhachHang;
-import Utils.Function;
-import NuocUong.QLNuocUong;
-import Ban.QLBan;
 import KhachHang.QLKhachHang;
+import NuocUong.QLNuocUong;
+import Utils.Function;
+import java.io.File;
+import java.util.Scanner;
 
 /**
  *
@@ -54,9 +54,25 @@ public class NguoiDung {
 
     }
 
-    public static void choseMenu() {
+    // public static void choseMenu() {
+    //     Scanner sc = new Scanner(System.in);
+    //     String str;
 
-    }
+    //     while (true) {
+    //         Function.clearScreen();
+    //         qlNuocUong.inDanhSach();
+    //         System.out.print("\tNhập ID nước bạn muốn đặt: ");
+    //         str = sc.nextLine();
+
+    //         if (Function.isEmpty(str)) {
+    //             System.out.println("\tVui lòng không để trống !");
+    //             continue;
+    //         }
+
+    //         // xử lí phần chọn nước
+    //         break;
+    //     }
+    // }
 
     public static void selectCustomer() {
 
@@ -66,6 +82,8 @@ public class NguoiDung {
         Scanner sc = new Scanner(System.in);
         String str;
         int number = 0;
+        int soluongkhach = 0;
+
         while (true) {
             Function.clearScreen();
             System.out.println(
@@ -80,6 +98,11 @@ public class NguoiDung {
             str = sc.nextLine();
             if (Function.isEmpty(str)) {
                 System.out.println("\tVui lòng không để trống !");
+                try {
+                    Thread.sleep(2500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             } else {
                 if (Function.isTrueNumber(str)) {
                     number = Integer.parseInt(str);
@@ -90,8 +113,58 @@ public class NguoiDung {
                             IDLink = account.checkLogin();
                             if (IDLink != "") {
                                 System.out.println("\tĐăng nhập thành công !");
-                                choseMenu();
-                                break;
+                                KHTaiCho temp = null;
+                                
+                                for (KhachHang kh : qlKhachHang.customerList) {
+                                    if (kh.getCustomerID().equals(IDLink)) {
+                                        temp = (KHTaiCho)kh;
+                                    }
+                                }
+
+                                while (true) { 
+                                    System.out.print("\tNhập số lượng khách: ");
+                                    str = sc.nextLine();
+
+                                    if (Function.isEmpty(str)) {
+                                        System.out.println("\tVui lòng không để trống !");
+                                        continue;
+                                    }
+
+                                    if (!Function.isTrueNumber(str)) {
+                                        System.out.println("\tVui lòng nhập số !");
+                                        continue;
+                                    }
+
+                                    temp.setNumberOfCustomer(Integer.parseInt(str));
+                                    temp.setTable(Ban.findTable(temp.getNumberOfCustomer()));
+                                    break;
+                                }
+
+                                if (temp.getTable() == null) {
+                                    System.out.println("\tKhông có bàn phù hợp!");
+
+                                } else {
+                                    while (true) {
+                                        Function.clearScreen();
+                                        qlNuocUong.inDanhSach();
+                                        System.out.println("\tBàn của bạn có ID: " + temp.getTable().getTableID());
+                                        System.out.print("\tNhập ID nước bạn muốn đặt: ");
+                                        str = sc.nextLine();
+                            
+                                        if (Function.isEmpty(str)) {
+                                            System.out.println("\tVui lòng không để trống !");
+                                            try {
+                                                Thread.sleep(2500);
+                                            } catch (InterruptedException e) {
+                                                e.printStackTrace();
+                                            }
+                                            continue;
+                                        }
+                            
+                                        // xử lí phần chọn nước
+                                        break;
+                                    }
+                                }
                             } else {
                                 System.out.println("\tĐăng nhập thất bại, đăng nhập lại ! !");
                                 try {
@@ -109,12 +182,27 @@ public class NguoiDung {
                             break;
                         } else {
                             System.out.println("\tVui lòng chọn từ 1 đến 3 !");
+                            try {
+                                Thread.sleep(2500);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
                         }
                     } else {
                         System.out.println("\tVui lòng chọn từ 1 đến 3 !");
+                        try {
+                            Thread.sleep(2500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
                 } else {
                     System.out.println("\tVui lòng nhập số !");
+                    try {
+                        Thread.sleep(2500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
@@ -122,6 +210,169 @@ public class NguoiDung {
     }
 
     public static void selectTwo() {
+        Scanner sc = new Scanner(System.in);
+        String str;
+
+        loop:
+        while (true) {
+            Function.clearScreen();
+            System.out.println(
+                    "\t=============================[Chức năng người Dùng mang đi]===============================");
+            System.out.printf("\t| %-87s |%n", "Chọn chức năng");
+            System.out.printf("\t| %-5s %-81s |%n", "1.", "Bắt đầu đặt món");
+            System.out.printf("\t| %-5s %-81s |%n", "2.", "Quay lại trang trước");
+            System.out.println(
+                    "\t==========================================================================================");
+            System.out.print("\tNhập lựa chọn của bạn: ");
+            str = sc.nextLine();
+
+            if (Function.isEmpty(str)) {
+                System.out.println("\tVui lòng không để trống !");
+                continue;
+            }
+
+            if (!Function.isTrueNumber(str)) {
+                System.out.println("\tVui lòng nhập số !");
+                continue;
+            }
+
+            switch (str) {
+                case "1":
+                loop1:
+                while (true) {
+                    Function.clearScreen();
+                    System.out.println(
+                        "\t=============================[Chức năng người Dùng mang đi]===============================");
+                    System.out.printf("\t| %-87s |%n", "Bạn đã từng mua hàng ở Spoce Coffee Shop chưa?");
+                    System.out.printf("\t| %-5s %-81s |%n", "1.", "Rồi");
+                    System.out.printf("\t| %-5s %-81s |%n", "2.", "Chưa từng");
+                    System.out.printf("\t| %-5s %-81s |%n", "3.", "Quay lại trang trước");
+                    System.out.println(
+                            "\t==========================================================================================");
+                    System.out.print("\tNhập lựa chọn của bạn: ");
+                    str = sc.nextLine();
+
+                    if (Function.isEmpty(str)) {
+                        System.out.println("\tVui lòng không để trống !");
+                        continue;
+                    }
+        
+                    if (!Function.isTrueNumber(str)) {
+                        System.out.println("\tVui lòng nhập số !");
+                        continue;
+                    }
+
+                    switch (str) {
+                        case "1":
+                        while (true) { 
+                            System.out.print("\tNhập mã khách hàng của bạn: ");
+                            str = sc.nextLine();
+
+                            if (Function.isEmpty(str)) {
+                                System.out.println("\tVui lòng không để trống !");
+                                continue;
+                            }
+                
+                            if (Function.isTrueNumber(str)) {
+                                System.out.println("\tVui lòng nhập chữ !");
+                                continue;
+                            }
+
+                            KhachHang temp = null;
+
+                            for (KhachHang kh : qlKhachHang.customerList) {
+                                if (kh.getCustomerID().equals(str)) {
+                                    temp = kh;
+                                    break;
+                                }
+                            }
+
+                            if (temp == null) {
+                                System.out.println("\tKhông tìm thấy khách hàng!");
+                                try {
+                                    Thread.sleep(2500);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                continue loop1;
+                            } else {
+                                while (true) {
+                                    Function.clearScreen();
+                                    qlNuocUong.inDanhSach();
+                                    System.out.print("\tNhập ID nước bạn muốn đặt: ");
+                                    str = sc.nextLine();
+                        
+                                    if (Function.isEmpty(str)) {
+                                        System.out.println("\tVui lòng không để trống !");
+                                        try {
+                                            Thread.sleep(2500);
+                                        } catch (InterruptedException e) {
+                                            e.printStackTrace();
+                                        }
+                                        continue;
+                                    }
+                        
+                                    // xử lí phần chọn nước
+                                    break;
+                                }
+                            }
+
+                            break;
+                        }
+                        break;
+
+                        case "2":
+                        while (true) {
+                            KhachHang temp = new KHMangDi();
+                            temp.nhapThongTin();
+                            qlKhachHang.customerList.add(temp);
+                            qlKhachHang.writeAll();
+
+                            while (true) { 
+                                Function.clearScreen();
+                                qlNuocUong.inDanhSach();
+                                System.out.print("\tNhập ID nước bạn muốn đặt: ");
+                                str = sc.nextLine();
+                    
+                                if (Function.isEmpty(str)) {
+                                    System.out.println("\tVui lòng không để trống !");
+                                    try {
+                                        Thread.sleep(2500);
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                    continue;
+                                }
+                    
+                                // xử lí phần chọn nước
+                                break;
+                            }
+                            break;
+                        }
+                        break;
+
+                        case "3":
+                        continue loop;
+
+                        default:
+                        System.out.println("\tVui lòng chọn từ 1 đến 3 !");
+                        continue;
+                    }
+
+                    break;
+                }
+                break;
+
+                case "2":
+                break;
+
+                default:
+                System.out.println("\tVui lòng chọn từ 1 đến 3 !");
+                continue;
+            }
+
+            break;
+        }
     }
 
     public static void Menu() {
